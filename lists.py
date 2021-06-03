f53c9649b9d2aebb225327c07546f9bd504e99d7
jfvergara <neo_ca@live.com.mx>
2021-06-02 20:51:38 -0500

    feat: The program is dividied in 2 modules. Also we import the definitions from one module to another

--- lists.py
+++
@@ -1,5 +0,0 @@
-my_list = ["January", "February", "March"]
-
-print(my_list[0])
-my_list.append("April")
-print(my_list)
--- main.py
+++ main.py
@@ -1,36 +1,11 @@
-
-def days_to_units(num_of_days, converson_unit):
-    if converson_unit == "hours":
-        return f"{num_of_days} days are {num_of_days * 24} hours"
-    elif converson_unit == "minutes":
-        return f"{num_of_days} days are {num_of_days * 24 * 60} minutes"
-    else:
-        return "Unsupported unit"
-
-
-
-def validate_and_execute():
-    try:
-        user_input_number = int(days_and_unit_dictionary["days"])
-        if user_input_number > 0:
-            calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
-            print(calculated_value)
-        elif user_input_number == 0:
-            print("0 is not a valid number")
-        elif user_input_number < 0:
-            print("I do not accept negative numbers, sorry!")
-    except ValueError:
-        print("Your input is not a number!!")
-
+from helper import validate_and_execute, user_input_message
 
 user_input = ""
 while user_input != "exit":
-    user_input = input("Hi, Enter number of days and conversion unit:\n")  # This is a env variable
+    user_input = input(user_input_message  )  # This is a env variable
     days_and_units = user_input.split(":")
     print(days_and_units)
     days_and_unit_dictionary = {"days": days_and_units[0], "unit": days_and_units[1]}
     print(days_and_unit_dictionary)
 
-    validate_and_execute()
-
-
+    validate_and_execute(days_and_unit_dictionary)

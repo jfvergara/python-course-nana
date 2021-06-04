1a6df31c8ef9611a7bb7598684eee4aa6e4cb2cb
jfvergara <neo_ca@live.com.mx>
2021-06-03 20:23:12 -0500

    feat: The time-till-deadline program is completed and working fine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from helper import validate_and_execute, user_input_message
+import os
 
-user_input = ""
+print(os.name)
+"""user_input = ""
 while user_input != "exit":
     user_input = input(user_input_message  )  # This is a env variable
     days_and_units = user_input.split(":")
@@ -8,4 +10,4 @@ while user_input != "exit":
     days_and_unit_dictionary = {"days": days_and_units[0], "unit": days_and_units[1]}
     print(days_and_unit_dictionary)
 
-    validate_and_execute(days_and_unit_dictionary)
+    validate_and_execute(days_and_unit_dictionary)"""
